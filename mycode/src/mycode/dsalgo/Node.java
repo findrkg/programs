ff53558ed6aac57ecb70f153e9b77a17d94ab8d0
RajkishorGupta <findrkg@gmail.com>
2017-09-15 16:04:08 +0530

    test

--- mycode/src/mycode/dsalgo/Node.java
+++ mycode/src/mycode/dsalgo/Node.java
@@ -1,8 +1,24 @@
 package mycode.dsalgo;
 
 public class Node {
+	private int data;
+	private Node next;
+	Node() {
+		this.data = 0;
+		this.next = null;
+	}
+	
+	Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Node ");
+		Node head = new Node(1, new Node(2, new Node(3, new Node(4, null))));
+		while(head != null) {
+			System.out.print(head.data +">");
+			head = head.next;			
+		}		
 	}
 }
