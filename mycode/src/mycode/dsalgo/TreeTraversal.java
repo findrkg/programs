eb6d6a2871718635423f6554fdc5b0e342ef49a1
RajkishorGupta <rajkishorgupta@fico.com>
2017-09-18 21:59:24 +0530

    TreeTraversal

--- mycode/src/mycode/dsalgo/TreeTraversal.java
+++ mycode/src/mycode/dsalgo/TreeTraversal.java
@@ -5,22 +5,45 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class TreeTraversal {
 
-	void printLevelOrder(TreeNode root) {
+	void levelOrder(TreeNode root) {
 		Queue<TreeNode> queue = new LinkedBlockingQueue<>();
 		queue.add(root);
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			TreeNode node = queue.poll();
-			System.out.print(node.data + " > ");
-			if(node.left != null)
+			System.out.print(node.data + " ");
+			if (node.left != null)
 				queue.add(node.left);
-			if(node.right != null)
+			if (node.right != null)
 				queue.add(node.right);
-			//queue.remove();
+		}
+	}
+
+	void preOrder(TreeNode root) {
+		if (root != null) {
+			// Visit the node-Printing the node data
+			System.out.print(root.data + " ");
+			preOrder(root.left);
+			preOrder(root.right);
+		}
+	}
+
+	public void postOrder(TreeNode root) {
+		if (root != null) {
+			postOrder(root.left);
+			postOrder(root.right);
+			// Visit the node by Printing the node data
+			System.out.printf("%d ", root.data);
 		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * 		1 
+		 * 	  2     3 
+		 * 4    5  6  7
+		 */
+
 		TreeNode root = new TreeNode(1);
 		root.left = new TreeNode(2);
 		root.right = new TreeNode(3);
@@ -28,9 +51,17 @@ public class TreeTraversal {
 		root.left.right = new TreeNode(5);
 		root.right.left = new TreeNode(6);
 		root.right.right = new TreeNode(7);
-		
+
 		TreeTraversal tree = new TreeTraversal();
-		tree.printLevelOrder(root);
+		System.out.print("Level Order : ");
+		tree.levelOrder(root);
+		System.out.println("");
+		System.out.print("Pre Order : ");
+		tree.preOrder(root);
+		System.out.println("");
+		System.out.print("Post Order : ");
+		tree.postOrder(root);
+
 	}
 
 }
