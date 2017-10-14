246730e872dd7c2a6ae2f61360ffe74da7e5abfe
RajkishorGupta <rajkishorgupta@fico.com>
2017-10-14 13:37:29 +0530

    is array sorted

--- mycode/src/mycode/dsalgo/IsArraySorted.java
+++ mycode/src/mycode/dsalgo/IsArraySorted.java
@@ -2,17 +2,32 @@ package mycode.dsalgo;
 
 public class IsArraySorted {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a[] = {1, 2, 3, 4, 5};
-		System.out.println(isSorted(a, 0));
-	}
-
-	public static boolean isSorted(int[] a, int index)
-	{
-		if(a.length == 1) {
-			return true;
-		}
-		return ((index < a.length-1) && a[index] > a[index+1]) ? false : isSorted(a, index+1);
-	}
+    // Function that returns 0 if a pair
+    // is found unsorted
+    static int arraySortedOrNot(int arr[], int n)
+    {
+        // Array has one or no element or the
+        // rest are already checked and approved.
+        if (n == 1 || n == 0)
+            return 1;
+      
+        // Unsorted pair found (Equal values allowed)
+        if (arr[n-1] <= arr[n-2])
+            return 0;
+      
+        // Last pair was sorted
+        // Keep on checking
+        return arraySortedOrNot(arr, n-1);
+    }
+     
+    // main function
+    public static void main (String[] args) 
+    {
+        int arr[] = {20, 23, 45, 78, 88};
+        int n = arr.length;
+        if (arraySortedOrNot(arr, n) != 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
