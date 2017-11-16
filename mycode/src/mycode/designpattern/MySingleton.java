828d35ae9c07a9f9d465c43886db2ebf17108837
RajkishorGupta <rajkishorgupta@fico.com>
2017-11-16 12:04:54 +0530

    MySingleton

--- mycode/src/mycode/designpattern/MySingleton.java
+++ mycode/src/mycode/designpattern/MySingleton.java
@@ -2,22 +2,46 @@ package mycode.designpattern;
 
 import java.io.Serializable;
 
-public class MySingleton implements Serializable {
+public class MySingleton implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
- 
-    private MySingleton() {
-        // private constructor
-    }
- 
-    private static class DemoSingletonHolder {
+
+    /**
+     * Bill Pugh Singleton Implementation
+     * When the singleton class is loaded, SingletonHelper class is not 
+     * loaded into memory and only when someone calls the getInstance method, 
+     * this class gets loaded and creates the Singleton class instance.
+     * @author RajkishorGupta
+     *
+     */
+    private static class SingletonHolder {
         public static final MySingleton INSTANCE = new MySingleton();
     }
+
+    private MySingleton() {
+    	if (SingletonHolder.INSTANCE != null) {
+            throw new IllegalStateException("Already instantiated");
+        }        // private constructor
+    }
+ 
  
     public static MySingleton getInstance() {
-        return DemoSingletonHolder.INSTANCE;
+        return SingletonHolder.INSTANCE;
     }
  
     protected Object readResolve() {
         return getInstance();
     }
+    
+    protected Object clone() throws CloneNotSupportedException {
+    	return getInstance();
+    }
+    
+    public static void main(String[] args) {
+    	MySingleton single2 = MySingleton.getInstance();
+    	System.out.println(single2.hashCode());
+    	MySingleton single = new MySingleton();
+    	System.out.println(single.hashCode());
+
+    }
+    
 }
